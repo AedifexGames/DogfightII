27bc3a5ceecf399d32c3c7a847c1831b03406ec3
AedifexGames <60152048+AedifexGames@users.noreply.github.com>
2025-11-08 05:19:45 -0600

    Added Movement Features
    Auto realignment, center gravitation, dash offset, etc

--- Assets/Assets/Code/Camera/CameraTarget.cs
+++ Assets/Assets/Code/Camera/CameraTarget.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
-
-public class CameraTarget : MonoBehaviour
+public class CameraTarget : SingletonBehaviour<CameraTarget>
 {
     [SerializeField] private Transform _target;
-    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _offset;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(_target.position.x + _offset.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(_target.position.x + _offset, transform.position.y, transform.position.z);
     }
+
+    public void SetOffset(float o) {  _offset = o; }
 }
--- Assets/Assets/Code/Player/Player.cs
+++ Assets/Assets/Code/Player/Player.cs
@@ -6,11 +6,15 @@ public class Player : MonoBehaviour
     #region Variables
     private InputManager _inputManager;
     private Rigidbody2D _rb;
+
+    //Movement Settings
     [SerializeField] private float _turnSpeed;
     [SerializeField] private float _baseMoveSpeed;
     private float _currentMoveSpeed;
     [SerializeField] private float _maxAngleOffset = 60f;
     private UnityEventOnTimer _projectileTimer;
+    [SerializeField] private float _realignmentTime;
+    private float _currRealignmentTime;
 
     //Charge Variables
     [SerializeField] private float _targetChargeAmount;
@@ -32,6 +36,18 @@ public class Player : MonoBehaviour
     private float _currentDashTime;
     private bool _dashing;
 
+    //Offset Control Variables
+    private CameraTarget _cameraTarget;
+    [SerializeField] private float _minOffset;
+    [SerializeField] private float _maxOffset;
+    [SerializeField] private float _targetSpeedForMinOffset;
+    [SerializeField] private float _targetSpeedForMaxOffset;
+    [SerializeField] private AnimationCurve _offsetLerp;
+
+    //Gravitation Variables
+    [SerializeField] private float _centerGravStrength = 5;
+    [SerializeField] private AnimationCurve _gravitationToCenter;
+
     //PLACEHOLDER
     private SpriteRenderer _spriteRenderer;
     #endregion
@@ -48,44 +64,76 @@ public class Player : MonoBehaviour
         _spriteRenderer.color = Color.orange;
 
         _currentMoveSpeed = _baseMoveSpeed;
+
+        _cameraTarget = CameraTarget.instance;
     }
 
     private void Update()
     {
+        // Set movement settings
         _rb.angularVelocity = _turnSpeed * _inputManager.TurnInput();
         _rb.linearVelocity = _currentMoveSpeed * transform.up;
         ClampAngle();
-        if (_charging && !_charged)
-        {
-            _currentChargeAmount += Time.deltaTime * _chargeSpeed;
-            if (_currentChargeAmount > _targetChargeAmount)
-            {
-                _charged = true;
-                _spriteRenderer.color = Color.green;
-            }
-            float dif = _cachedMoveSpeed - _chargeMinSpeed;
-            _currentMoveSpeed = _chargeMinSpeed + _chargeSlowDown.Evaluate(_currentChargeAmount / _targetChargeAmount) * dif;
-        }
+        ClampYAxis();
+        RealignPath();
 
-        if (_dashing)
-        {
-            _currentDashTime += Time.deltaTime;
-            if (_currentDashTime > _totalDashTime)
-            {
-                EndDash();
-            }
-            //Update the speed with the curve
-            float dif = _cachedMoveSpeed - _baseMoveSpeed;
-            _currentMoveSpeed = _baseMoveSpeed + _dashLifetimeSpeed.Evaluate(_currentDashTime / _totalDashTime) * dif;
-            Debug.Log("Dashing at the speed of: " + _currentMoveSpeed);
-        }
+        // Apply gravitation forces
+        _rb.linearVelocityY += GravitationToCenter();
+
+        // Handle dashing and charging
+        Charge();
+        Dash();
+        CalculateOffset(); // move forward with speed
+
+
     }
 
     private void ClampAngle()
     {
         float z = transform.rotation.eulerAngles.z;
         if (z > 180f) z -= 360f;
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Clamp(z, -90f - _maxAngleOffset, -90f + _maxAngleOffset));
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x, 
+            transform.rotation.eulerAngles.y, 
+            Mathf.Clamp(z, -90f - _maxAngleOffset, -90f + _maxAngleOffset)
+        );
+    }
+
+    private void ClampYAxis()
+    {
+        float edgesDist = 10;
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Clamp(transform.position.y, -edgesDist, edgesDist),
+            transform.position.z
+        );
+    }
+
+    private void RealignPath()
+    {
+        if (_inputManager.TurnInput() == 0 && _inputManager.SpacebarPressed())
+        {
+            // Get direction and change
+            float rotateDegree = 0.5f;
+            float angleOffset = transform.rotation.eulerAngles.z - 270;
+
+            if (angleOffset == 0) return;
+
+            float pointedDirection = angleOffset / Math.Abs(angleOffset);
+
+            // Check edge case
+            rotateDegree = (Math.Abs(angleOffset) < rotateDegree) ? Math.Abs(angleOffset) : rotateDegree;
+            
+            // Calculate change
+            float z = transform.rotation.eulerAngles.z + (rotateDegree * (-pointedDirection));
+
+            // Apply change
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y,
+                z
+            );
+        }
     }
 
     private void SpacebarStart()
@@ -110,6 +158,49 @@ public class Player : MonoBehaviour
         _projectileTimer.Unpause();
         _charging = false;
         _charged = false;
+    }
+
+    private void Charge()
+    {
+        // If charging (not charged)
+        if (_charging && !_charged)
+        {
+            // Increase charge
+            _currentChargeAmount += Time.deltaTime * _chargeSpeed;
+
+            // Finish charging
+            if (_currentChargeAmount > _targetChargeAmount)
+            {
+                _charged = true; // prevent further increase
+                _spriteRenderer.color = Color.green; // PROTOTYPE marker
+            }
+
+            // Slow down movement
+            float dif = _cachedMoveSpeed - _chargeMinSpeed;
+            _currentMoveSpeed = _chargeMinSpeed + _chargeSlowDown.Evaluate(_currentChargeAmount / _targetChargeAmount) * dif;
+        }
+    }
+
+    private void Dash()
+    {
+        // Current dashing
+        if (_dashing)
+        {
+            // Set movement settinggs
+            _rb.angularVelocity = (_turnSpeed / 2) * _inputManager.TurnInput(); // half turning speed
+
+            // Count down dash duration
+            _currentDashTime += Time.deltaTime;
+            if (_currentDashTime > _totalDashTime)
+            {
+                EndDash();
+            }
+
+            // Speed up movement and dropoff
+            float dif = _cachedMoveSpeed - _baseMoveSpeed;
+            _currentMoveSpeed = _baseMoveSpeed + _dashLifetimeSpeed.Evaluate(_currentDashTime / _totalDashTime) * dif;
+            //Debug.Log("Dashing at the speed of: " + _currentMoveSpeed);
+        }
     }
     
     private float GetDashSpeed()
@@ -139,9 +230,44 @@ public class Player : MonoBehaviour
 
     private void EndDash()
     {
-        Debug.Log("Ending Dash");
+        //Debug.Log("Ending Dash");
         _dashing = false;
         _currentMoveSpeed = _baseMoveSpeed;
         _spriteRenderer.color = Color.orange;
+    }
+
+    private void CalculateOffset()
+    {
+        // Velocities
+        float v_0 = _baseMoveSpeed; // base speed
+        float v_x = _currentMoveSpeed; // current speed
+        float v_m = _targetSpeedForMinOffset; // goal speed
+
+        // Positions
+        float B = _minOffset; // goal position
+        float A = _maxOffset;
+
+        // Differences
+        float dif_pos = B - A; // distance to goal position
+        float dif_vel = v_m - v_0; // difference between current and goal speeds
+
+        // Times
+        float time = v_x - v_0; // amount faster than base speed
+        float time_norm = time / dif_vel; // time normalized [0,1]
+
+        // Set offset
+        _cameraTarget.SetOffset(A + (_offsetLerp.Evaluate(time_norm) * dif_pos));
+    }
+
+    private float GravitationToCenter()
+    {
+        float y = transform.position.y;
+        float edgeDist = 10;
+        float yOffset = Math.Abs(y);
+        float yOffsetNorm = yOffset / edgeDist;
+
+        float flipFactor = y / Math.Abs(y);
+
+        return _centerGravStrength * _gravitationToCenter.Evaluate(yOffsetNorm) * -flipFactor;
     }
 }
