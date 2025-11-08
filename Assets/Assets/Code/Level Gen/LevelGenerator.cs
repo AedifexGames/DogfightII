2df6726d1439360655460dec072ddaa490243026
AedifexGames <60152048+AedifexGames@users.noreply.github.com>
2025-11-08 17:08:24 -0600

    Auto Delete Offscreen Terrain

--- Assets/Assets/Code/Level Gen/LevelGenerator.cs
+++ Assets/Assets/Code/Level Gen/LevelGenerator.cs
@@ -10,10 +10,12 @@ enum CardID
 public class LevelGenerator : SingletonBehaviour<LevelGenerator>
 {
     private float _currentLevelCursorX = 35.5f;
+    private float _deletionOffsetFromCursor = -100f;
     private static readonly float[] _yOffsets = { -7.5f, -2.5f, 2.5f, 7.5f };
 
     private List<int> _currentColumn;
     private List<int> _lastColumn;
+    private List<Transform> _createdColumns;
 
     [SerializeField] private List<CellDataSO> _cards = new List<CellDataSO>();
 
@@ -21,27 +23,43 @@ public class LevelGenerator : SingletonBehaviour<LevelGenerator>
     {
         _lastColumn = new List<int> { 0, 0, 0, 0 };
         _currentColumn = new List<int> { 0, 0, 0, 0 };
+        _createdColumns = new List<Transform>();
         for (int i = 0; i < 5; i++) GenerateColumn();
     }
-
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < _createdColumns.Count; i++)
+        {
+            if (_createdColumns[i].position.x < _currentLevelCursorX + _deletionOffsetFromCursor)
+            {
+                GameObject toDestroy = _createdColumns[i].gameObject;
+                _createdColumns.RemoveAt(i);
+                Destroy(toDestroy);
+                i--;
+            }
+        }
+    }
     public void GenerateColumn()
     {
-        
+        GameObject parentObj = new GameObject("Parent at: " + _currentLevelCursorX);
+        Transform parent = parentObj.transform;
+        _createdColumns.Add(parent);
+        parent.transform.position = Vector3.right * _currentLevelCursorX;
         for (int i = 0; i < 4; i++)
         {
-            Vector2 pos = new Vector2(_currentLevelCursorX, _yOffsets[i]);
+            Vector2 pos = new Vector2(0, _yOffsets[i]);
             int horizontalNeighbor = _lastColumn[i];
             int verticalNeighbor = (i > 0) ? _currentColumn[i - 1] : 0;
             bool[] bordering = new bool[_cards.Count];
             for (int j = 0; j < _cards.Count; j++) { bordering[j] = (horizontalNeighbor == _cards[j].Id || verticalNeighbor == _cards[j].Id); }
-            _currentColumn[i] = GenerateLevelCell(pos, bordering);
+            _currentColumn[i] = GenerateLevelCell(pos, bordering, parent);
         }
         _lastColumn = _currentColumn;
         for (int i = 0; i < 4; i++) _currentColumn[i] = 0;
         _currentLevelCursorX += 5;
     }
 
-    private int GenerateLevelCell(Vector2 pos, bool[] bordering)
+    private int GenerateLevelCell(Vector2 pos, bool[] bordering, Transform parent)
     {
         Debug.Log(bordering);
         float totalWeight = CalculateTotalWeight(bordering);
@@ -54,9 +72,9 @@ public class LevelGenerator : SingletonBehaviour<LevelGenerator>
             {
                 GameObject prefab = _cards[i].GetPrefab();
                 if (prefab == null) return _cards[i].Id;
-                GameObject obj = GameObject.Instantiate(prefab);
+                GameObject obj = GameObject.Instantiate(prefab, parent);
                 if (obj == null) return _cards[i].Id;
-                obj.transform.position = pos;
+                obj.transform.localPosition = pos;
                 return _cards[i].Id;
             }
             previousWeight += _cards[i].Weight(bordering[i]);
