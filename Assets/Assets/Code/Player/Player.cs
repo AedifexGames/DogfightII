2b52e6d318ea8a753f72584bf628b6b0875e366c
AedifexGames <60152048+AedifexGames@users.noreply.github.com>
2025-11-08 02:17:36 -0600

    DASH WORKS (no offset shift)

--- Assets/Assets/Code/Player/Player.cs
+++ Assets/Assets/Code/Player/Player.cs
@@ -3,23 +3,38 @@ using System;
 using UnityEngine.XR;
 public class Player : MonoBehaviour
 {
+    #region Variables
     private InputManager _inputManager;
     private Rigidbody2D _rb;
     [SerializeField] private float _turnSpeed;
-    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _baseMoveSpeed;
+    private float _currentMoveSpeed;
     [SerializeField] private float _maxAngleOffset = 60f;
     private UnityEventOnTimer _projectileTimer;
 
+    //Charge Variables
     [SerializeField] private float _targetChargeAmount;
     [SerializeField] private float _chargeSpeed = 1;
     private float _currentChargeAmount;
     private bool _charging = false;
     private bool _charged = false;
+    [SerializeField] private AnimationCurve _chargeSlowDown;
+    [SerializeField] private float _chargeMinSpeed;
+    private float _cachedMoveSpeed;
 
+    //Dash Variables
+    [SerializeField] private float _fullyChargedDashSpeed;
+    [SerializeField] private float _minChargeToDash;
+    [SerializeField] private float _partialChargeDashDifference;
+    [SerializeField] private float _baseDashSpeed;
+    [SerializeField] private AnimationCurve _dashLifetimeSpeed;
+    [SerializeField] private float _totalDashTime;
+    private float _currentDashTime;
+    private bool _dashing;
 
     //PLACEHOLDER
     private SpriteRenderer _spriteRenderer;
-
+    #endregion
 
     private void Start()
     {
@@ -31,12 +46,14 @@ public class Player : MonoBehaviour
 
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.color = Color.orange;
+
+        _currentMoveSpeed = _baseMoveSpeed;
     }
 
     private void Update()
     {
         _rb.angularVelocity = _turnSpeed * _inputManager.TurnInput();
-        _rb.linearVelocity = _moveSpeed * transform.up;
+        _rb.linearVelocity = _currentMoveSpeed * transform.up;
         ClampAngle();
         if (_charging && !_charged)
         {
@@ -46,6 +63,21 @@ public class Player : MonoBehaviour
                 _charged = true;
                 _spriteRenderer.color = Color.green;
             }
+            float dif = _cachedMoveSpeed - _chargeMinSpeed;
+            _currentMoveSpeed = _chargeMinSpeed + _chargeSlowDown.Evaluate(_currentChargeAmount / _targetChargeAmount) * dif;
+        }
+
+        if (_dashing)
+        {
+            _currentDashTime += Time.deltaTime;
+            if (_currentDashTime > _totalDashTime)
+            {
+                EndDash();
+            }
+            //Update the speed with the curve
+            float dif = _cachedMoveSpeed - _baseMoveSpeed;
+            _currentMoveSpeed = _baseMoveSpeed + _dashLifetimeSpeed.Evaluate(_currentDashTime / _totalDashTime) * dif;
+            Debug.Log("Dashing at the speed of: " + _currentMoveSpeed);
         }
     }
 
@@ -58,27 +90,58 @@ public class Player : MonoBehaviour
 
     private void SpacebarStart()
     {
+        EndDash();
         _projectileTimer.Pause();
         _currentChargeAmount = 0;
         _charging = true;
         _charged = false;
         _spriteRenderer.color = Color.yellow;
+        _cachedMoveSpeed = _currentMoveSpeed;
     }
 
     private void SpacebarEnd()
     {
+        _currentMoveSpeed = _baseMoveSpeed;
         _spriteRenderer.color = Color.orange;
-        if (_charged)
+        if (_currentChargeAmount >= _minChargeToDash)
         {
-            Dash();
+            StartDash();
         }
         _projectileTimer.Unpause();
         _charging = false;
         _charged = false;
     }
-
-    private void Dash()
+    
+    private float GetDashSpeed()
     {
+        //Partially charged (over threshold)
+        if (_currentChargeAmount > _minChargeToDash && !_charged)
+        {
+            float progressTime = _currentChargeAmount - _minChargeToDash;
+            return _baseDashSpeed + (progressTime * (_partialChargeDashDifference / (_targetChargeAmount - _minChargeToDash)));
+        }
+        //Fully charged
+        else if (_charged)
+        {
+            return _fullyChargedDashSpeed;
+        }
+        return _baseMoveSpeed;
+    }
 
+    private void StartDash()
+    {
+        _currentMoveSpeed = GetDashSpeed();
+        _dashing = true;
+        _currentDashTime = 0;
+        _cachedMoveSpeed = _currentMoveSpeed;
+        _spriteRenderer.color = Color.blue;
+    }
+
+    private void EndDash()
+    {
+        Debug.Log("Ending Dash");
+        _dashing = false;
+        _currentMoveSpeed = _baseMoveSpeed;
+        _spriteRenderer.color = Color.orange;
     }
 }
