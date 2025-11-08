e7734464b35d90b00e7340c874408fa628cb91c5
AedifexGames <60152048+AedifexGames@users.noreply.github.com>
2025-11-08 01:01:02 -0600

    Clamp player angle

--- Assets/Assets/Code/Player/Player.cs
+++ Assets/Assets/Code/Player/Player.cs
@@ -7,27 +7,36 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rb;
     [SerializeField] private float _turnSpeed;
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _maxAngleOffset = 60f;
     private void Start()
     {
         _inputManager = InputManager.instance;
-        _inputManager.onSpacebarPressed += Spacebar;
+        _inputManager.onSpacebarPressed += SpacebarStart;
+        _inputManager.onSpacebarRelease += SpacebarEnd;
         _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        TurnShip();
         _rb.angularVelocity = _turnSpeed * _inputManager.TurnInput();
         _rb.linearVelocity = _moveSpeed * transform.up;
+        ClampAngle();
     }
 
-    public void TurnShip()
+    private void ClampAngle()
     {
-
+        float z = transform.rotation.eulerAngles.z;
+        if (z > 180f) z -= 360f;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Clamp(z, -90f - _maxAngleOffset, -90f + _maxAngleOffset));
     }
 
-    public void Spacebar()
+    private void SpacebarStart()
     {
         
+    }
+
+    private void SpacebarEnd()
+    {
+
     }
 }
