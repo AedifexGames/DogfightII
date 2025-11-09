97a27ad81f3905f41021dd58f096a8d704502550
AedifexGames <60152048+AedifexGames@users.noreply.github.com>
2025-11-09 04:41:39 -0600

    Boss shoots lasers

---
+++ Assets/Assets/Code/Generic/LookTowardTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardTransform : MonoBehaviour
+{
+    public Transform target;
+    public float angleOffset;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float AngleRad = Mathf.Atan2((target.position.y) - transform.position.y, (target.position.x) - transform.position.x);
+
+        float AngleDeg = (180 / Mathf.PI) *AngleRad + angleOffset;
+
+        transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+}
